7e5dd4b3701ab0d6aa7ec8e3b686db5f16d75c7d
Steven <pozosteven65@gmail.com>
2020-05-06 21:46:51 -0500

    Hello

---
+++ script..js
@@ -0,0 +1,16 @@
+// JavaScript source code
+// Llaves {}[]
+var json = {
+    nombre: "Cristina",
+    edad: 21,
+    soltera: true,
+    fecha: new Date ("1998/11/07"),
+}
+
+json.nombre = "nuevo"
+json.direccion = "Tumbaco"
+delete json.direccion
+console.log(json)
+/*console.error()
+console.info()
+console.warn()*/
