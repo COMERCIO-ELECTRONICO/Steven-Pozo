f17199b77df04a255988e81cae697ddbeba0e202
Steven <pozosteven65@gmail.com>
2020-05-08 12:13:39 -0500

    string

---
+++ string.js
@@ -0,0 +1,21 @@
+var nombre = 'steven'
+//console.log(nombre.lengt);
+//console.log(nombre.indexof('e'));
+//console.log(nombre.search('i'));
+//console.log(nombre.slice(1,3));
+//console.log(nombre.substr(1,3));
+//console.log(nombre.toLocaleUpperCase());
+//console.log(nombre.toUpperCase());
+var texto ='hola mundo :v'
+console.log(texto.replace('hol','hello'))
+console.log(nombre.concat(',',nombre)
+console.log(`${texto} mas mi nombre: ${nombre}`)
+console.log(texto.trim())
+console.log(texto.split())
+console.log(nombre.charAt(1))
+
+
+
+
+
+
