ffd40a3a13596925872fffae7038461dbb12b8e3
Steven <pozosteven65@gmail.com>
2020-05-19 21:57:12 -0500

    rutas-angular

---
+++ rutas-angular/src/app/app.routes.ts
@@ -0,0 +1,42 @@
+import { UsuarioRoutingModule } from './usuario/usuario-routing.module';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './inicio/home/home.component';
+import { NoEncontradoComponent } from './inicio/no-encontrado/no-encontrado.component';
+import { LoginComponent } from './login/login.component';
+const rutas: Routes = [
+    {
+        path: 'inicio',
+        component: HomeComponent
+
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'usuario',
+        loadChildren: () => import('./usuario/usuario.module')
+            .then(m => m.UsuarioModule)
+    },
+    {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: NoEncontradoComponent
+
+    }
+];
+@NgModule({
+    imports: [RouterModule.forRoot(rutas)],
+    exports: [RouterModule]
+})
+
+
+
+export class AppRoutesComponent {
+
+}
