cebd6eb871b4fe9c7afd85fc8c72b68cc68833c0
Steven <pozosteven65@gmail.com>
2020-05-24 18:32:01 -0500

    ejercicio

---
+++ ejercicio/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { AppRoutes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NoEncontradoComponent,
+    IniciarSesionComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutes,
+    BrowserAnimationsModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
