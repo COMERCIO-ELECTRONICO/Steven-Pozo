1d8130bf5d871060c56575f7b71bcacb37f41213
Steven <pozosteven65@gmail.com>
2020-05-29 01:26:01 -0500

    conexion

---
+++ back-end/api/models/Usuario.js
@@ -0,0 +1,46 @@
+/**
+ * Usuario.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre: {
+      type: 'string',
+      required: true
+    },
+    correo: {
+      type: 'string'
+    },
+    edad: {
+      type: 'number'
+    },
+    esCasado: {
+      type: 'boolean'
+    },
+    mascota: {
+      collection: 'mascota',
+      via: 'idUsuario'
+    }
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
